0926a06076b5d466337e22d3b1c8ee4574d99aff
GrandChariot <65334052+GrandChariot@users.noreply.github.com>
2020-05-24 15:22:42 +0900

    Add files via upload

---
+++ printf 함수 활용 예제.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("제 이름은 김재연입니다 \n");
+	printf("제 나이는 24살이구요. \n");
+	printf("제가 사는 곳의 동호수는 %d-%d입니다 \n", 102, 602);
+
+	printf("%dx%d=%d \n", 4, 5, 20);
+	printf("%dx%d=%d \n", 7, 9, 63);
+
+	return 0;
+}
